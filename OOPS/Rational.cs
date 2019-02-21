a7d58c74a3fb715307f5875ffe6d958682940100
Hariprasad Kottarathil <khprasad123@gmail.com>
2019-02-21 20:46:10 +0800

    Completed All exercises in Day 1 OOPS Excercises any doubts ask ???

---
+++ OOPS/Rational.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class Rational
+    {
+        int numerator;
+        int denomenator;
+
+        public Rational() { numerator = 1; denomenator= 1;}  //default constructor
+        public Rational(int x,int y)                        //parameterised constructor
+        {
+            numerator = x;
+            denomenator = y;
+        }
+        public void Reduce()
+        {
+            int x = numerator;
+            int y = denomenator;
+            int temp_x = 1, temp_y=1;
+            
+            for(int i = 2; i < x / 2; i++)
+            {
+                while((x%i)==0 && (y % i) == 0)
+                {
+                    x = x / i;
+                    y = y / i;
+                }
+            }
+            numerator = x;  denomenator = y;
+        }
+        public void Print()
+        {
+            Console.WriteLine("\t\t\t\t" + numerator + " / " + denomenator);
+        }
+        public Rational Multiply(Rational Y)
+        {
+            Rational result = new Rational();
+            result.numerator=this.numerator * Y.numerator;
+            result.denomenator=this.denomenator * Y.denomenator;
+            return result;
+        }
+    }
+}
---
+++ OOPS/RationalTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    class RationalTest
+    {
+        static void Main()
+        {
+            Rational r1 = new Rational(8,16);
+          //  r1.Print();
+            r1.Reduce();
+            r1.Print();
+
+            Rational r2 = new Rational(30, 3);
+
+           
+           r2.Reduce();
+            r2.Print();
+
+            Console.WriteLine("After Multiplication :");
+            (r2.Multiply(r1)).Print();
+
+            Rational ans = r2.Multiply(r1);
+            ans.Reduce();
+            ans.Print();
+
+        }
+    }
+}
